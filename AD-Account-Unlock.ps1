fb124c559a6624343de47645b8feb55d0ff67f2b
J Bancroft <127392547+jbancroft88@users.noreply.github.com>
2023-05-05 13:13:00 +0200

    Delete AD-Account-Unlock.ps1

--- AD-Account-Unlock.ps1
+++
@@ -1,8 +0,0 @@
-$usr = Read-Host ("Enter Username")
-Get-ADUser $usr -Property LockedOut
-$prompt = Read-Host ("Unlock User (y/n)?")
-if ("y" -eq $prompt)
-{
-    Unlock-ADAccount $usr
-    Get-ADUser $usr -Property LockedOut
-}
